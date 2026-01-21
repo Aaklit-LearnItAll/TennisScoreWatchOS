54bf64ac241003f8e18e2790cf114967a0c85deb
Aaklit <aaklit@gmail.com>
2026-01-21 13:57:48 +0000

    Initial Commit

---
+++ Tenniscore Watch App/TenniscoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenniscoreApp.swift
+//  Tenniscore Watch App
+//
+//  Created by Aaklit Taneja on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tenniscore_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
